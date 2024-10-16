5108367cf604b04da1973b5e60547caa3d7e1cca
hiba-u <coder.hiba@gmail.com>
2024-10-16 13:42:33 +0530

    refactor

--- notes-app.js
+++ notes-app.js
@@ -1,43 +1,30 @@
-notes = [{
-    title: 'Note 1',
-    text: 'This is note 1'
-}, {
-    title: 'Example 2',
-    text: 'This is example 2'
-},{
-    title: 'Sample note 3',
-    text: 'This is sample note 3'
-}]
+const notes = getSavedNotes()
 
 const filters = {
     searchText: '',
 }
 
-
-const renderNotes = function(notes, filters){
-
-    const filteredNotes = notes.filter(function(note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = ''
-
-    filteredNotes.forEach(function(note){
-        const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
-
 renderNotes(notes, filters)
 
+// Create note
+document.querySelector("#create-note").addEventListener('click', function(e){
+    notes.push({
+        title: '',
+        body: ''
+    })
+
+    saveNotes(notes)
+    renderNotes(notes, filters)
+})
+
+// Filter notes by title
 document.querySelector('#search-text').addEventListener('input', function(e){
     
     filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
 
+// Sort notes
 document.querySelector('#sort-by').addEventListener('change', function(e){
     console.log(e.target.value)
 })
